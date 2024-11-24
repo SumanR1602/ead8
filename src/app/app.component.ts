308dc2b8173e353713b4ed8fd94983fa1cf6ca45
SumanR1602 <suman160204@gmail.com>
2024-11-25 01:22:57 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'exp8';
-}
+import { Component } from '@angular/core'; 
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms'; 
+import { CommonModule } from '@angular/common'; 
+@Component({ 
+selector: 'app-root', 
+standalone: true, 
+imports: [CommonModule, ReactiveFormsModule], 
+templateUrl: './app.component.html', 
+styleUrls: ['./app.component.css'] 
+}) 
+export class AppComponent { 
+editDataForm: FormGroup; 
+constructor() { 
+this.editDataForm = new FormGroup({ 
+name: new FormControl('John Doe'), 
+email: new FormControl('john.doe@example.com'), 
+}); 
+}onSubmit() { 
+console.log('Form Data:', this.editDataForm.value); 
+} 
+} 
